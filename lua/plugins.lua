00d1b1c642d9f068f6dbf950423e77af599fafa1
mspraggs <mspraggs@thoughtmachine.net>
2023-05-15 12:06:54 +0100

    feat: Packer bootstrap

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,4 +1,9 @@
 -- This file can be loaded by calling `lua require("plugins")` from your init.vim
+local install_path = vim.fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
+if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
+	vim.fn.system({ 'git', 'clone', 'https://github.com/wbthomason/packer.nvim', install_path })
+	vim.cmd.packadd('packer.nvim')
+end
 
 -- Only required if you have packer configured as `opt`
 vim.cmd [[packadd packer.nvim]]
