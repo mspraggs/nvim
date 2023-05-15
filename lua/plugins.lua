c0fa43f05cbe4899197eb51337438aadea9701fa
Matt Spraggs <matthew.spraggs@gmail.com>
2023-05-15 18:55:31 +0100

    feat: Add auto-save plugin

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -102,4 +102,14 @@ require("packer").startup(function(use)
 		'tpope/vim-sleuth',
 	})
 
+	use({
+		"Pocco81/auto-save.nvim",
+		config = function()
+			require("auto-save").setup {
+				-- your config goes here
+				-- or just leave it empty :)
+			}
+		end,
+	})
+
 end)
