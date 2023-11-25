438131b546c05018b6a77040a82cf7a059c1044a
Matt Spraggs <matthew.spraggs@gmail.com>
2023-11-25 22:57:37 +0000

    Update telescope

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -13,7 +13,7 @@ require("packer").startup(function(use)
 	use "wbthomason/packer.nvim"
 
 	use {
-		"nvim-telescope/telescope.nvim", tag = "0.1.1",
+		"nvim-telescope/telescope.nvim", tag = "0.1.3",
 		requires = {"nvim-lua/plenary.nvim"},
 		config = function()
 			require("plugins.telescope")
