bb5c51fe5a2f4e669a514010b8daab6e54dbc3a5
Matt Spraggs <matthew.spraggs@gmail.com>
2025-06-12 10:12:56 +0100

    feat: Restore cursor location on file load

--- lua/options.lua
+++ lua/options.lua
@@ -26,3 +26,9 @@ vim.api.nvim_create_autocmd("BufWritePre", {
 	group = format_sync_grp,
 })
 
+vim.api.nvim_create_autocmd({ "BufReadPost" }, {
+    pattern = { "*" },
+    callback = function()
+        vim.api.nvim_exec('silent! normal! g`"zv', false)
+    end,
+})
