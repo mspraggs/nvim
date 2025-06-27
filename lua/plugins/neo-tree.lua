10de77d60a37e5cae5d4953e2d9d7cbb441c27c4
mspraggs <mspraggs@thoughtmachine.net>
2025-06-27 13:35:41 +0100

    feat: Improve neo-tree config

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,1 +1,13 @@
-vim
+vim.keymap.set({ 'n' }, '<leader>nf', ':Neotree toggle<CR>')
+
+require('neo-tree').setup({
+	close_if_last_window = true,
+	event_handlers = {
+		{
+			event = "file_opened",
+			handler = function ()
+				require('neo-tree.command').execute({ action = "close" })
+			end,
+		},
+	}
+})
