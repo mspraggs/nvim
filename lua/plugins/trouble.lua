d8e1623709f6a7ff4db7cc8fe25b899bc711c712
Matt Spraggs <matthew.spraggs@gmail.com>
2025-07-23 23:17:41 +0100

    fix: Update Trouble config

--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -1,34 +1,39 @@
 return {
 	{
-		'folke/trouble.nvim',
-		config = function ()
-			require('trouble').setup {
-				icons = false,
-				fold_open = "v", -- icon d for open folds
-				fold_closed = ">", -- icon d for closed folds
-				indent_lines = false, -- add an indent guide below the fold icons
-				signs = {
-					-- icons / text d for a diagnostic
-					error = "error",
-					warning = "warn",
-					hint = "hint",
-					information = "info"
-				},
-				_diagnostic_signs = false -- enabling this will the signs defined in your lsp client
-			}
-
-			local trouble = require("trouble.sources.telescope")
-
-			local telescope = require("telescope")
-
-			telescope.setup {
-				defaults = {
-					mappings = {
-						i = { ["<c-t>"] = trouble.open },
-						n = { ["<c-t>"] = trouble.open },
-					},
-				},
-			}
-		end
-	},
+		"folke/trouble.nvim",
+		opts = {}, -- for default options, refer to the configuration section for custom setup.
+		cmd = "Trouble",
+		keys = {
+			{
+				"<leader>xx",
+				"<cmd>Trouble diagnostics toggle<cr>",
+				desc = "Diagnostics (Trouble)",
+			},
+			{
+				"<leader>xX",
+				"<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+				desc = "Buffer Diagnostics (Trouble)",
+			},
+			{
+				"<leader>cs",
+				"<cmd>Trouble symbols toggle focus=false<cr>",
+				desc = "Symbols (Trouble)",
+			},
+			{
+				"<leader>cl",
+				"<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+				desc = "LSP Definitions / references / ... (Trouble)",
+			},
+			{
+				"<leader>xL",
+				"<cmd>Trouble loclist toggle<cr>",
+				desc = "Location List (Trouble)",
+			},
+			{
+				"<leader>xQ",
+				"<cmd>Trouble qflist toggle<cr>",
+				desc = "Quickfix List (Trouble)",
+			},
+		},
+	}
 }
