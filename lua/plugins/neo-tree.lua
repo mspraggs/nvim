f2c632746d4761e511838e3a9c406d2a7e1d4439
mspraggs <mspraggs@thoughtmachine.net>
2025-09-16 10:15:06 +0100

    feat: Git shortcuts for NeoTree

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -20,7 +20,22 @@ return {
 							require('neo-tree.command').execute({ action = "close" })
 						end,
 					},
-				}
+				},
+				window = {
+					mappings = {
+						["<space>"] = {
+							"toggle_node",
+							nowait = false, -- disable `nowait` if you have existing combos starting with this char that you want to use
+						},
+						["gA"] = "git_add_all",
+						["gu"] = "git_unstage_file",
+						["ga"] = "git_add_file",
+						["gr"] = "git_revert_file",
+						["gc"] = "git_commit",
+						["gp"] = "git_push",
+						["gg"] = "git_commit_and_push"
+					}
+				},
 			})
 		end,
 	},
